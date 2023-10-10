a5edd7a8e4200d6914f9db1a60d430506d2a2b3a
william vage <william.vage@gmail.com>
2023-10-10 15:28:21 +0200

    Adding server and few client changes

---
+++ client/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+export default App;
+
+function App() {
+    return<>
+        <main>
+            <h1>Welcome to My Movie Database!</h1>
+            <li><a href={"#"}>List movies </a></li>
+            <li><a href={"#"}>Create a new movie </a></li>
+        </main>
+        <footer>By: William Våge </footer>
+
+    </>
+
+}
--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import "./Application.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello React</h1>);
+root.render(<App/>);
